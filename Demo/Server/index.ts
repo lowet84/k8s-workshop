2f8e3a1621969c0164a5b0b9bb01116d9fa94034
Fredrik Löwenhamn <lowet84@gmail.com>
2019-10-19 18:19:58 +0200

    Wip

--- Demo/Server/index.ts
+++ Demo/Server/index.ts
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser'
 const wss = new Server({ port: 8080 })
 
 var app = express()
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.post('/api', (req, res) => {
   wss.clients.forEach(function each(client) {
