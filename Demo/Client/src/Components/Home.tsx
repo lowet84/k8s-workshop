77477a10aa41165af93c1c0deff0bf276dd565e1
Fredrik Löwenhamn <lowet84@gmail.com>
2019-10-22 09:24:07 +0200

    Wip

--- Demo/Client/src/Components/Home.tsx
+++ Demo/Client/src/Components/Home.tsx
@@ -5,6 +5,25 @@ import Card from 'antd/es/card'
 import Layout from 'antd/es/layout'
 import Row from 'antd/es/row'
 import Col from 'antd/es/col'
+import { randomColors } from '../colors'
+
+const hashCode = function(str: string) {
+  var hash = 0,
+    i,
+    chr
+  if (str.length === 0) return hash
+  for (i = 0; i < str.length; i++) {
+    chr = str.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
+    hash |= 0
+  }
+  return Math.abs(hash)
+}
+
+const colorFromString = (str: string) => {
+  var index = hashCode(str.toString()) % randomColors.length
+  return randomColors[index]
+}
 
 export const Home = () => {
   var messages = useSelector(getGroupedMessages)
@@ -14,7 +33,13 @@ export const Home = () => {
         <Row gutter={16}>
           {messages.map((message, index) => (
             <Col key={index} md={12} lg={6} style={{ marginBottom: '16px' }}>
-              <Card title={message.name} style={{ height: '250px' }}>
+              <Card
+                title={message.name}
+                style={{
+                  height: '250px',
+                  border: `5px solid ${colorFromString(message.name)}`
+                }}
+              >
                 {message.hosts.map((host, hindex) => (
                   <div key={hindex}>
                     <div style={{ display: 'flex', flexDirection: 'row' }}>
@@ -26,7 +51,7 @@ export const Home = () => {
                             margin: '5px',
                             width: '10px',
                             height: '10px',
-                            background: 'black'
+                            background: colorFromString(id.id)
                           }}
                         ></div>
                       ))}
